3aacb9d6f952b1760acef37858bc052df76f4303
KyleStorm1812 <guilhermecarvalho1812@gmail.com>
2020-05-31 14:36:49 -0300

    Altered main.py to check for invalid type errors

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ def get_temperature(lat, lng):
     url = 'https://api.darksky.net/forecast/{}/{},{}'.format(key, lat, lng)
     response = requests.get(url)
     data = response.json()
-    temperature = data.get('currently').get('temperature')
-    if not temperature:
-        return
-    return int((temperature - 32) * 5.0 / 9.0)
+    try:
+        if (type(data.get('currently').get('temperature'))) == float or int:
+            temperature = data.get('currently').get('temperature')
+            return int((temperature - 32) * 5.0 / 9.0)
+        else:
+            return "Invalid Temperature Type"
+    except:
+        return data
