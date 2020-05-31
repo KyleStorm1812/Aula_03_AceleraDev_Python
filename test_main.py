908b39a796eb374a581c66c17ec1736b343c9eb6
KyleStorm1812 <guilhermecarvalho1812@gmail.com>
2020-05-31 16:48:54 -0300

    Attempted to introduce more tests, but the challenge did not accept then. So, instead, I've improved the current ones,

--- test_main.py
+++ test_main.py
@@ -3,10 +3,6 @@ import pytest
 from mock import patch
 
 coordinates_test_1 = [(-14.235004, -51.92528, 16)]
-coordinates_test_2 = [(-14.235004, -51.92528, {"code": 400,
-                                               "error": "The given location is invalid. Please try again."})]
-coordinates_test_3 = [(None, -51.92528, {"code": 400,
-                                         "error": "Invalid data type. Please try again."})]
 
 
 @pytest.mark.parametrize("lag, lng, expected", coordinates_test_1)
